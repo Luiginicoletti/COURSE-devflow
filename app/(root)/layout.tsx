e9586991aae1ebe93d442f32bb69b38f94b62f17
Nicoletti <callmenicoletti@gmail.com>
2023-10-15 10:44:33 -0300

    NavPart1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,19 @@
+import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative">
-      {children}
-      <div className="fdex">
+    <main className="relative background-light850_dark100">
+      <Navbar />
+      <div className="flex">
         LeftSidebar
-        <section className="flex min-h-screen flex-1 flex-col bg-amber-700 px-6 pb-6 pt-36 max-md:pb-14 sm:px-14"></section>
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
         RightSidebar
       </div>
-    </main>
+      Toaster
+    </main> 
   );
 };
 
---
+++ components/shared/navbar/teste.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  // Defina suas propriedades aqui
+}
+
+const ComponentName: React.FC<Props> = ({}) => {
+  return (
+    <div>
+      <h1>ComponentName</h1>
+    </div>
+  );
+};
+
+export default ComponentName;
